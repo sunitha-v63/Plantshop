342257e3e74bcbc6b592728553e91424c573a41d
sunitha-d23 <sunithagopal06@gmail.com>
2025-05-05 18:14:05 +0530

    a

--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -6,6 +6,7 @@ import Footer from './Footer';
 function MainHeader() {
     return (
         <>
+        {/* header-footer */}
             <Navbar />
             <Outlet />
             <Footer />
